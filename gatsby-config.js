5d6c193f699bcc52c2fe50030f19e386e48a1ad1
Darek Mazur <dev@darekmazur.it>
2023-07-08 00:06:02 +0200

    Setup project: Linter, Prettier, Husky

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,47 +1,52 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
-let env = process.env.NODE_ENV || 'development';
-
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+require('dotenv').config({
+	path: `.env.${process.env.NODE_ENV}`,
+});
 
 module.exports = {
-  siteMetadata: {
-    title: `nerdistry_gatsbyjs`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [`${process.env.GATSBY_TRACKING_ID}`],
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        pluginConfig: {
-          head: false,
-          respectDNT: true,
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-          delayOnRouteUpdate: 0,
-        },
-      },
-    }
-  ]
+	siteMetadata: {
+		title: `nerdistry_gatsbyjs`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: [
+		'gatsby-plugin-styled-components',
+		'gatsby-plugin-image',
+		'gatsby-plugin-sitemap',
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				icon: 'src/images/icon.png',
+			},
+		},
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: './src/images/',
+			},
+			__key: 'images',
+		},
+		{
+			resolve: `gatsby-plugin-google-gtag`,
+			options: {
+				// You can add multiple tracking ids and a pageview event will be fired for all of them.
+				trackingIds: [`${process.env.GATSBY_TRACKING_ID}`],
+				gtagConfig: {
+					optimize_id: 'OPT_CONTAINER_ID',
+					anonymize_ip: true,
+					cookie_expires: 0,
+				},
+				pluginConfig: {
+					head: false,
+					respectDNT: true,
+					exclude: ['/preview/**', '/do-not-track/me/too/'],
+					delayOnRouteUpdate: 0,
+				},
+			},
+		},
+	],
 };
