488217632ad2adc70d99fbbd7b03470330bffe87
Darek Mazur <dev@darekmazur.it>
2023-07-07 23:48:19 +0200

    Initial Gatsby configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,18 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+let env = process.env.NODE_ENV || 'development';
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `nerdistry_gatsbyjs`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
@@ -18,5 +24,24 @@ module.exports = {
       "path": "./src/images/"
     },
     __key: "images"
-  }]
+  },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [`${process.env.GATSBY_TRACKING_ID}`],
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        pluginConfig: {
+          head: false,
+          respectDNT: true,
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          delayOnRouteUpdate: 0,
+        },
+      },
+    }
+  ]
 };
