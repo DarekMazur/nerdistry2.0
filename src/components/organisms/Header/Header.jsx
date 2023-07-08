053aafa449d5858f5266aa371228bfcd0a043b95
Darek Mazur <dev@darekmazur.it>
2023-07-09 01:10:36 +0200

    Create basic components

---
+++ src/components/organisms/Header/Header.jsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+
+function Header() {
+	return (
+		<header>
+			<div>
+				<span>Logo</span>
+				<nav>
+					<ul>
+						<li>
+							<p>Lorem</p>
+							<p>Lorem Ipsum Dolor Sit Amet</p>
+						</li>
+						<li>
+							<p>Ipsum</p>
+							<p>Lorem Ipsum Dolor Sit Amet</p>
+						</li>
+						<li>
+							<p>Dolor</p>
+							<p>Lorem Ipsum Dolor Sit Amet</p>
+						</li>
+						<li>
+							<p>Sit Amet</p>
+							<p>Lorem Ipsum Dolor Sit Amet</p>
+						</li>
+					</ul>
+				</nav>
+				<span>Switcher</span>
+			</div>
+			<div>
+				<h2>Nerdistry.</h2>
+				<p>
+					Z notatnika młodego deva, czyli od juniora do zera. Czy tam na
+					odwrót... ;)
+				</p>
+			</div>
+			<div>
+				<span>Icon</span>
+			</div>
+		</header>
+	);
+}
+
+export default Header;
