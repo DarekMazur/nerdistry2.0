d653874062b7a2fd504ed431ca1b2abc329f12d0
Darek Mazur <dev@darekmazur.it>
2023-07-19 20:14:13 +0200

    Remove import fonts from css

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -7,7 +7,6 @@ import { far } from '@fortawesome/free-regular-svg-icons';
 library.add(fab, fas, far);
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@400;700&family=Montserrat+Subrayada&family=Montserrat:ital@0;1&family=Notable&family=Share+Tech+Mono&display=swap');
   
   html {
     box-sizing: border-box;
