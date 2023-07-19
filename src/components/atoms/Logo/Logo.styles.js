96287567f0dacd7a4688892dcc4fe665b92db32c
Darek Mazur <dev@darekmazur.it>
2023-07-19 17:42:37 +0200

    Fix mobile first styles

--- src/components/atoms/Logo/Logo.styles.js
+++ src/components/atoms/Logo/Logo.styles.js
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 
 export const StyledLogo = styled.div`
 	svg {
-		width: 10rem;
-		height: 10rem;
+		width: 6rem;
+		height: 6rem;
 	}
 
-	@media screen and (max-width: ${({ theme }) => theme.breakPoints.mobile}) {
+	@media screen and (min-width: ${({ theme }) => theme.breakPoints.tablet}) {
 		svg {
-			width: 6rem;
-			height: 6rem;
+			width: 10rem;
+			height: 10rem;
 		}
 	}
 `;
--- src/components/organisms/Header/Header.styles.js
+++ src/components/organisms/Header/Header.styles.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import background from '../../../assets/images/headerBackground.jpg';
 
 export const StyledHeader = styled.header`
-	//height: 655px;
 	width: 100%;
 	background: url(${background}) no-repeat center/cover;
 	color: ${({ theme }) => theme.colors.white};
