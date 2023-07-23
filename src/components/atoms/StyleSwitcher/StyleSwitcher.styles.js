2872f96a8f452d985236672506760ae421e1acea
Darek Mazur <dev@darekmazur.it>
2023-07-23 03:39:38 +0200

    Fix switcher styles

--- src/components/atoms/StyleSwitcher/StyleSwitcher.styles.js
+++ src/components/atoms/StyleSwitcher/StyleSwitcher.styles.js
@@ -1,41 +1,57 @@
 import styled from 'styled-components';
 
 export const StyledStyleSwitcher = styled.button`
-  height: 3.5rem;
-  width: 10rem;
-  border-radius: 1.75rem;
-  border: none;
-  cursor: pointer;
-  background-color: ${({ theme, isDark }) =>
-		isDark ? theme.colors.white : theme.colors.black};
-  position: relative;
-  transition: background-color 0.5s ease-in-out;
-  
-    svg {
-      height: 3rem;
-      position: absolute;
-      top: 0.25rem;
-	  transition: all 0.5s ease-in-out;
+	height: 2.5rem;
+	width: 8rem;
+	border-radius: 1.75rem;
+	border: none;
+	cursor: pointer;
+	background-color: ${({ theme, isDark }) =>
+		isDark ? theme.colors.black : theme.colors.white};
+	position: relative;
+	transition: background-color 0.2s 0.3s ease-in-out;
+	margin-bottom: 1rem;
 
-    path {
-      stroke: ${({ theme, isDark }) =>
-				isDark ? theme.colors.black : theme.colors.white};
-      fill: ${({ theme, isDark }) =>
-				isDark ? theme.colors.black : theme.colors.white};
-      transform-origin: 50% 50%;
-    }
-    
-	  &:nth-of-type(1) {
-		opacity: ${({ isDark }) => (isDark ? `0` : `1`)};
-		left: ${({ isDark }) => (isDark ? `calc(100% - 32.5px)` : `0.25rem`)};
-		transform: ${({ isDark }) => (isDark ? `rotate(180deg)` : `rotate(0)`)};
-	  }
-    
-      &:nth-of-type(2) {
-        opacity: ${({ isDark }) => (isDark ? `1` : `0`)};
-        left: ${({ isDark }) => (isDark ? `0.25rem` : `calc(100% - 32.5px)`)};
-		transform: ${({ isDark }) => (isDark ? `rotate(0)` : `rotate(180deg)`)};
-      }
-    }
-  }
+	svg {
+		height: 2rem;
+		position: absolute;
+		top: 0.25rem;
+		left: ${({ isDark }) => (isDark ? `calc(100% - 22.5px)` : `0.25rem`)};
+		transition:
+			left 0.5s ease-in-out,
+			opacity 0.2s 0.3s ease-in-out;
+
+		path {
+			stroke: ${({ theme, isDark }) =>
+				isDark ? theme.colors.white : theme.colors.black};
+			fill: ${({ theme, isDark }) =>
+				isDark ? theme.colors.white : theme.colors.black};
+			transform-origin: 50% 50%;
+			transition:
+				stroke 0.2s 0.3s ease-in-out,
+				fill 0.2s 0.3s ease-in-out;
+		}
+
+		&:nth-of-type(1) {
+			opacity: ${({ isDark }) => (isDark ? `0` : `1`)};
+		}
+
+		&:nth-of-type(2) {
+			opacity: ${({ isDark }) => (isDark ? `1` : `0`)};
+		}
+	}
+
+	@media screen and (min-width: ${({ theme }) => theme.breakPoints.tablet}) {
+		height: 3.5rem;
+		width: 10rem;
+
+		svg {
+			height: 3rem;
+			left: ${({ isDark }) => (isDark ? `calc(100% - 32.5px)` : `0.25rem`)};
+		}
+	}
+
+	@media screen and (min-width: ${({ theme }) => theme.breakPoints.desktop}) {
+		margin-bottom: 0;
+	}
 `;
