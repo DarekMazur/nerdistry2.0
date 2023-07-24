765f4d6b41a1d53fb98e028d10da67bbe33f5076
Darek Mazur <dev@darekmazur.it>
2023-07-24 14:17:49 +0200

    Add posted data

--- src/components/molecules/SinglePostExcerpt/SinglePostExcerpt.jsx
+++ src/components/molecules/SinglePostExcerpt/SinglePostExcerpt.jsx
@@ -1,26 +1,31 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import { getTextExcerpt } from '../../../utils/helpers/getTextExcerpt';
+import { getDateFormat } from '../../../utils/helpers/getDateFormat';
 
-const SinglePostExcerpt = ({ post }) => (
-	<div>
+const SinglePostExcerpt = ({ post }) => {
+	const publishedDate = new Date(post.attributes.publishedAt);
+
+	return (
 		<div>
-			<img
-				src={post.attributes.CoverImage.data.attributes.formats.medium.url}
-				alt={post.attributes.Title}
-			/>
+			<div>
+				<img
+					src={post.attributes.CoverImage.data.attributes.formats.medium.url}
+					alt={post.attributes.Title}
+				/>
+			</div>
+			<p>
+				<span>posted on</span> {getDateFormat(publishedDate)}
+			</p>
+			<h4>{post.attributes.Title}</h4>
+			<p>
+				{post.attributes.Description
+					? post.attributes.Description
+					: getTextExcerpt(post.attributes.Content, 300)}
+			</p>
 		</div>
-		<p>
-			<span>posted on</span> data
-		</p>
-		<h4>{post.attributes.Title}</h4>
-		<p>
-			{post.attributes.Description
-				? post.attributes.Description
-				: getTextExcerpt(post.attributes.Content, 300)}
-		</p>
-	</div>
-);
+	);
+};
 
 export default SinglePostExcerpt;
 
@@ -31,6 +36,7 @@ SinglePostExcerpt.propTypes = {
 			Title: PropTypes.string,
 			Description: PropTypes.string,
 			Content: PropTypes.string,
+			publishedAt: PropTypes.string,
 			CoverImage: PropTypes.shape({
 				data: {
 					attributes: {
