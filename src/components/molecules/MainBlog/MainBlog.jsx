192154926ec760f8cffc1b6d23e061d4a10a7c9a
Darek Mazur <dev@darekmazur.it>
2023-07-24 13:18:24 +0200

    Create component sketch

---
+++ src/components/molecules/MainBlog/MainBlog.jsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+
+const MainBlog = () => (
+	<>
+		<div>
+			<div>
+				<span>image</span>
+			</div>
+			<p>
+				<span>posted on</span> data
+			</p>
+			<h4>Lorem Ipsum</h4>
+			<p>
+				Lorem ipsum dolor sit amet consectetur. Eleifend commodo etiam ultrices
+				augue nunc mauris pretium. Adipiscing volutpat placerat vitae eu gravida
+				ac. Eu blandit euismod mollis faucibus.
+			</p>
+		</div>
+		<div>
+			<div>
+				<span>image</span>
+			</div>
+			<p>
+				<span>posted on</span> data
+			</p>
+			<h4>Lorem Ipsum</h4>
+			<p>
+				Lorem ipsum dolor sit amet consectetur. Eleifend commodo etiam ultrices
+				augue nunc mauris pretium. Adipiscing volutpat placerat vitae eu gravida
+				ac. Eu blandit euismod mollis faucibus.
+			</p>
+		</div>
+		<div>
+			<div>
+				<span>image</span>
+			</div>
+			<p>
+				<span>posted on</span> data
+			</p>
+			<h4>Lorem Ipsum</h4>
+			<p>
+				Lorem ipsum dolor sit amet consectetur. Eleifend commodo etiam ultrices
+				augue nunc mauris pretium. Adipiscing volutpat placerat vitae eu gravida
+				ac. Eu blandit euismod mollis faucibus.
+			</p>
+		</div>
+		<div>
+			<div>
+				<span>image</span>
+			</div>
+			<p>
+				<span>posted on</span> data
+			</p>
+			<h4>Lorem Ipsum</h4>
+			<p>
+				Lorem ipsum dolor sit amet consectetur. Eleifend commodo etiam ultrices
+				augue nunc mauris pretium. Adipiscing volutpat placerat vitae eu gravida
+				ac. Eu blandit euismod mollis faucibus.
+			</p>
+		</div>
+		<p>Read more</p>
+	</>
+);
+
+export default MainBlog;
