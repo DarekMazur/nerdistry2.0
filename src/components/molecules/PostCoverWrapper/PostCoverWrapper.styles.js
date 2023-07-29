b94a69717fdeac14c2018f1a34d34df531f7bac2
Darek Mazur <dev@darekmazur.it>
2023-07-29 20:50:05 +0200

    Create base of post content element

--- src/components/molecules/PostCoverWrapper/PostCoverWrapper.styles.js
+++ src/components/molecules/PostCoverWrapper/PostCoverWrapper.styles.js
@@ -10,4 +10,20 @@ export const StyledPostCoverWrapper = styled.div`
 		width: 100vw;
 		object-fit: cover;
 	}
+
+	@media screen and (min-width: ${({ theme }) => theme.breakPoints.tablet}) {
+		height: 49rem;
+
+		img:first-of-type {
+			height: 49rem;
+		}
+	}
+
+	@media screen and (min-width: ${({ theme }) => theme.breakPoints.desktop}) {
+		height: 67rem;
+
+		img:first-of-type {
+			height: 67rem;
+		}
+	}
 `;
