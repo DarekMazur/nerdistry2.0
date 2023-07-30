ea895325d8a85aa69229dac6d2af435838a72cf7
Darek Mazur <dev@darekmazur.it>
2023-07-30 23:40:33 +0200

    Fix date display format

--- src/utils/helpers/getDateFormat.js
+++ src/utils/helpers/getDateFormat.js
@@ -1,6 +1,6 @@
 export const getDateFormat = (date) => {
 	const formatted = new Date(date);
-	return `${String(formatted.getDate()).padStart(2, '0')}.${String(
+	return `${String(formatted.getDate())}.${String(
 		formatted.getMonth() + 1
 	).padStart(2, '0')}.${formatted.getFullYear()}`;
 };
