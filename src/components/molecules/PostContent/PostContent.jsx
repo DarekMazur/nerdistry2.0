7e2e884211797b0842d6e303d4372cbc4df5d8f2
Darek Mazur <dev@darekmazur.it>
2024-05-14 18:48:34 +0200

    Highlight syntax with prism

--- src/components/molecules/PostContent/PostContent.jsx
+++ src/components/molecules/PostContent/PostContent.jsx
@@ -1,13 +1,22 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import Prism from 'prismjs';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import { StyledPostContent } from './PostContent.styles';
+import 'prismjs/themes/prism-tomorrow.css';
+import 'prismjs/components/prism-typescript';
 
-const PostContent = ({ content }) => (
-	<StyledPostContent>
-		<ReactMarkdown>{content}</ReactMarkdown>
-	</StyledPostContent>
-);
+const PostContent = ({ content }) => {
+	useEffect(() => {
+		Prism.highlightAll();
+	}, []);
+
+	return (
+		<StyledPostContent>
+			<ReactMarkdown>{content}</ReactMarkdown>
+		</StyledPostContent>
+	);
+};
 
 export default PostContent;
 
--- src/components/molecules/PostContent/PostContent.styles.js
+++ src/components/molecules/PostContent/PostContent.styles.js
@@ -4,8 +4,21 @@ export const StyledPostContent = styled.div`
 	font-size: ${({ theme }) => theme.fontSize.mobile.m};
 	margin: 2rem;
 
+	a {
+		color: ${({ theme }) => theme.colors.lightBlue};
+		text-decoration: none;
+
+		&:hover {
+			text-decoration: underline;
+		}
+	}
+
 	img {
 		width: 100%;
+	}
+
+	pre[class*='language-'] {
+		padding: 3rem;
 	}
 
 	@media screen and (min-width: ${({ theme }) => theme.breakPoints.tablet}) {
