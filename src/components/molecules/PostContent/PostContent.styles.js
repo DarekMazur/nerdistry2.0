d1f6ba6c304d168ff685a66f134d9f6e50e1119d
Darek Mazur <dev@darekmazur.it>
2024-05-14 18:15:06 +0200

    Style article images

--- src/components/molecules/PostContent/PostContent.styles.js
+++ src/components/molecules/PostContent/PostContent.styles.js
@@ -4,6 +4,10 @@ export const StyledPostContent = styled.div`
 	font-size: ${({ theme }) => theme.fontSize.mobile.m};
 	margin: 2rem;
 
+	img {
+		width: 100%;
+	}
+
 	@media screen and (min-width: ${({ theme }) => theme.breakPoints.tablet}) {
 		font-size: ${({ theme }) => theme.fontSize.tablet.m};
 		margin: 6rem;
